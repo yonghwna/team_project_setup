97fd35f59843fa977b031f87c50c136ebedc9869
yonghwna <yongdev0273@gmail.com>
2024-09-16 13:57:47 +0900

    Feat: ESLint설정 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
-import './index.css';
-
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
+import { store } from './app/store.ts';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(<App />);
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
